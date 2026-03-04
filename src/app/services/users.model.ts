9a5c19a1e7d966899b5106bd23ca1aaec3e1d459
MMD1358 <xdeadx1358@gmail.com>
2026-03-04 13:39:58 +0100

    Completo css y arreglos

---
+++ src/app/services/users.model.ts
@@ -0,0 +1,14 @@
+export interface Users {
+  id: string;
+  email: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// interface Prueba<tipo,tipo2> {
+//     datos: tipo;
+//     datos2: tipo2;
+// }
+
+// const prueba: Prueba<number,boolean> = {datos:5, datos2:true}
